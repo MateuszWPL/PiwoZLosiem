9513af2966b3907687ac6360324a34eb9e53b028
Miłosz Sidor <mainloszek@gmail.com>
2025-10-11 19:47:25 +0200

    Slider

--- frontend/src/router/index.js
+++ frontend/src/router/index.js
@@ -2,11 +2,13 @@ import { createRouter, createWebHistory } from 'vue-router'
 import HomeView from '../views/HomeView.vue'
 import UserPage from '../pages/RegisterPage.vue'
 import Dashboard from '@/pages/Dashboard.vue'
+import Welcome from '@/pages/Welcome.vue'
 
 const routes = [
   { path: '/', name: 'Home', component: HomeView },
   { path: '/register', name: 'Register', component: UserPage },
   { path: '/dashboard', name: 'Dashboard', component: Dashboard },
+  { path: '/welcome', name: 'Welcome', component: Welcome },
 ]
 
 const router = createRouter({
