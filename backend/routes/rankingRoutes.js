c8b30b4d06105891e27e09c526ff211c07933586
Kacper Mróz <kacper.mroz1@gmail.com>
2025-10-18 03:07:21 +0200

    dokonczenie prac przy rankingu - dodanie wyswietlania statystyk dla aktualnie zalogowanego uzytkownika "w kafelku", dodana obsluga przycisku oraz style.

--- backend/routes/rankingRoutes.js
+++ backend/routes/rankingRoutes.js
@@ -1,9 +1,13 @@
 import express from "express";
-import { getRanking } from "../controllers/rankingController.js";
+import { getRanking, getCurrentUserRanking } from "../controllers/rankingController.js";
+import { protect } from "../middleware/authMiddleware.js";
 
 const router = express.Router();
 
 // ranking globalny
 router.get("/ranking/:period", getRanking);
+
+// ranking aktualnego użytkownika
+router.get("/ranking/:period/current-user", protect, getCurrentUserRanking);
 
 export default router;
