469930a00e2530f0be55c0e7e74f689064edb3ac
Miłosz Sidor <mainloszek@gmail.com>
2025-10-25 13:14:27 +0200

    usunięcie vue-devtools z configu

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,16 +1,14 @@
 import { fileURLToPath, URL } from 'node:url'
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
-import vueDevTools from 'vite-plugin-vue-devtools'
 
-export default defineConfig(({ mode }) => ({ 
+export default defineConfig({
   plugins: [
     vue(),
-    mode === 'development' && vueDevTools(),
   ],
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url)),
     },
   },
-}))
+})
