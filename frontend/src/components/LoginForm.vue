aaf91d984481bfa0b1c2a994d2881a030bd4ccd4
Miłosz Sidor <mainloszek@gmail.com>
2025-10-25 13:27:01 +0200

    Zmiana odwolania do API

--- frontend/src/components/LoginForm.vue
+++ frontend/src/components/LoginForm.vue
@@ -102,7 +102,7 @@ const router = useRouter()
 const submit = async () => {
   errorMessage.value = ''
   try {
-    const res = await axios.post('http://localhost:5000/api/auth/login', {
+    const res = await apiClient.post('/auth/login', {
       email: email.value,
       password: password.value,
     })
