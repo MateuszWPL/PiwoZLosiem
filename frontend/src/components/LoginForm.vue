d3870247b7c620283d5b52b8fd115c8a7a676394
pamix00 <skkfos@gmail.com>
2025-11-06 19:05:04 +0100

    v4

--- frontend/src/components/LoginForm.vue
+++ frontend/src/components/LoginForm.vue
@@ -107,7 +107,28 @@ const submit = async () => {
       password: password.value,
     })
 
+    const token = res.data.token
     localStorage.setItem('token', res.data.token)
+
+    const locationStore = useLocationStore()
+  
+
+    const resUser = await axios.get('/users/me', {
+      headers: { Authorization: `Bearer ${token}` }
+    })
+
+    const userData = resUser.data
+    locationStore.setUserData({
+      _id: userData._id,
+      firstName: userData.firstName,
+      lastName: userData.lastName,
+      age: userData.age,
+      name: `${userData.firstName} ${userData.lastName}`
+    })
+    locationStore.setStatus(userData.status)
+
+    locationStore.initializeSocket(token)
+    locationStore.startLocationTracking()
 
     if (!res.data.isProfileComplete) {
       router.push('/rejestracja-uzupelnienie')
