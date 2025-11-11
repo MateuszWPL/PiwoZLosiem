f9a85ff9f598781b152aa1e8681156e0ffa3d614
mkosinski <m.kosinskki@gmail.com>
2025-11-11 16:40:31 +0100

    zmiana czasu tokenu

--- backend/controllers/authController.js
+++ backend/controllers/authController.js
@@ -6,7 +6,7 @@ import { v4 as uuidv4 } from "uuid";
 
 // 🔒 Token zawiera tylko ID użytkownika
 const generateToken = (id) => {
-  return jwt.sign({ id }, process.env.JWT_SECRET, { expiresIn: "7d" });
+  return jwt.sign({ id }, process.env.JWT_SECRET, { expiresIn: "2h" });
 };
 
 // 🧩 Rejestracja użytkownika
